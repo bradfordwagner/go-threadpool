05b449b40e01ae084f544710a577edd7f8d10bd9
bradfordwagner <wagner.bradford@gmail.com>
2022-10-13 10:55:56 -0400

    Initialize

---
+++ threadpool_internal_test.go
@@ -0,0 +1,13 @@
+package threadpool
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Threadpool", func() {
+	It("will test things", func() {
+		Expect(true).To(BeTrue())
+		println("hi friends")
+	})
+})
